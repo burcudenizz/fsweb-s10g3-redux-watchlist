3bd008cc8a2b4a8af4ee609ce69d68b5a6f8ec8a
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-04-26 16:48:30 +0300

    proje tamamlandı.Esnek görevler kaldı.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import reducer from "./reducers";
+import { createLogger } from "redux-logger";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-import { BrowserRouter } from "react-router-dom"
+const logger = createLogger({
+  diff: true,
+  collapsed: true,
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const depo = createStore(reducer, applyMiddleware(logger));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={depo}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
@@ -17,3 +28,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+/*
+- turuncu kutu: 
+-- kütüphaneleri eklemek
+-- Provider, createStore, reducer, başlangıç değerleri
+
+- store içinden verileri okumak - useSelector
+- store içindeki verileri değiştirmek - useDispatch
+*/
